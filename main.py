2ec69f097c3e3ce17a13012428fe7b61bcbf8ede
Proxy <proxywastaken@riseup.net>
2022-09-30 17:54:55 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests as r
+import time
+
+
+last_mail = None
+url = 'https://www.1secmail.com/api/v1/'
+get_mail = \
+'?action=genRandomMailbox&count=1'
+
+
+res = r.request("GET", url+get_mail)\
+.json()
+
+print(res[0])
+
+mail = res[0].split('@')
+
+get_messages = \
+    '?action=getMessages&login='\
+        +f'{mail[0]}&domain={mail[1]}'
+        
+read_message = \
+    '?action=readMessage&login='\
+        +f'{mail[0]}&domain={mail[1]}'
+        
+        
+while(True):
+    time.sleep(5)
+    res = r.request("GET", \
+        url+get_messages).json()
+    
+    
+    
+    if len(res) > 0 and last_mail != res[0]:
+        print(res[0])
+        last_mail = res[0]
+        get_data = f'&id={last_mail["id"]}'
+        res = r.request("GET", \
+            url+read_message+get_data).json()
+        data = res['htmlBody']
+        
+        for line in data.split('\n'):
+            print(line)
+    
